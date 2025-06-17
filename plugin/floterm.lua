df26faba4c47b92dbb6ce48f1b2251e28ce65b01
danielgayon <danisegui19@gmail.com>
2025-06-17 20:34:56 +0200

    adding a keymap

--- plugin/floterm.lua
+++ plugin/floterm.lua
@@ -5,3 +5,7 @@ vim.api.nvim_create_user_command("Floterm", function()
 end, {})
 
 vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+-- Map <leader>tt to toggle the terminal (in normal and terminal mode)
+vim.keymap.set({ "n", "t" }, "<leader>tt", function()
+  require("floaterm").toggle_terminal()
+end, { desc = "Toggle floating terminal", silent = true })
